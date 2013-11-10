79e027963a053a2f748a37263635ccfb98ab3f41
Joel Frederico <joelfred@slac.stanford.edu>
2013-11-10 01:57:48 -0800

    Silenced things

--- E200_cam_E_cal.m
+++ E200_cam_E_cal.m
@@ -3,7 +3,7 @@ function E_axis = E200_cam_E_cal(data,y)
 	% spectrometer magnet strength, and ...
 	E0 = 20.35;
 	
-	B_spect = data.raw.scalars.LI20_LGPS_3330_BDES.dat{1}
+	B_spect = data.raw.scalars.LI20_LGPS_3330_BDES.dat{1};
 	etay = 62e-3 * (B_spect/20.35) * (9.28/10.33);
-	E_axis = -etay*E0./y
+	E_axis = -etay*E0./y;
 end
