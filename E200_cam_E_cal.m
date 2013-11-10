1708e4cff2532b4db7c8df67af8234ea2b760cff
Joel Frederico <joelfred@slac.stanford.edu>
2013-11-09 21:04:46 -0800

    Partial attempt at ELANEX calibration

---
+++ E200_cam_E_cal.m
@@ -0,0 +1,9 @@
+function E_axis = E200_cam_E_cal(data,y)
+	% Calibration depends on good orbit through quads,
+	% spectrometer magnet strength, and ...
+	E0 = 20.35;
+	
+	B_spect = data.raw.scalars.LI20_LGPS_3330_BDES.dat{1}
+	etay = 62e-3 * (B_spect/20.35) * (9.28/10.33);
+	E_axis = -etay*E0/y
+end
