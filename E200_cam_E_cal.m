78de37da9161d9693b35afe5a4c379c7345195a1
Joel Frederico <joelfred@slac.stanford.edu>
2013-11-11 19:10:53 -0800

    Should be working...?

--- E200_cam_E_cal.m
+++ E200_cam_E_cal.m
@@ -1,4 +1,4 @@
-function E_axis = E200_cam_E_cal(data,y)
+function E_axis = E200_cam_E_cal(data,y,res)
 	% ====================================
 	% Calibration depends on good orbit through quads,
 	% spectrometer magnet strength, and ...?
@@ -11,6 +11,7 @@ function E_axis = E200_cam_E_cal(data,y)
 	% y0 determined from ROI'd centroid of E200_11469
 	% step 6, image 32.
 	y0 = 476.5593;
+	res=res*1e-6;
 	
 	% ====================================
 	% Get the magnet strength
@@ -21,12 +22,14 @@ function E_axis = E200_cam_E_cal(data,y)
 	% The dispersion at y0
 	% ====================================
 	etay = 62e-3 * (B_spect/20.35) * (9.28/10.33);
+	yc = y0 + etay/res
 
 	% ====================================
 	% Convert to energy axis
 	% E_axis = -etay*E0./(y-y0);
 	% E_axis = E0./( 1-((y-y0)/etay) );
-	E_axis = E0*( 1- (y-y0)/etay );
+	% E_axis = E0*( 1- (y-y0)/etay );
+	E_axis = -etay*E0./( (y-yc)*res );
 	display(-etay*(0.01)+y0);
 	display(-etay*(-0.01)+y0);
 end
